bd8e7781f43463b7bb785f60af8f25e0ac556240
rishpatil123 <rishpatil123@gmail.com>
2022-11-15 16:12:07 +0530

    My Project

---
+++ src/main/java/com/example/demo/SpringGitJpaNewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitJpaNewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitJpaNewProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+class Student {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("New Spring Jpa Project");
+	}
+
+}
